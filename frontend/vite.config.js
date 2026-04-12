203a14017313fc713d35d6e6157f440a566e2c37
sod3 <mudassir.muneer123@gmail.com>
2026-04-12 16:34:19 +0500

    frontend1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
-});
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://entrepreneurship-society.vercel.app',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+  }
+})
